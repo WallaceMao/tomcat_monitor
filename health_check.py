72f8d7991fe7bfc2d8ba2bb4943f69bc9efa1086
Wallace Mao <maowenqiang0752@163.com>
2017-12-28 17:13:30 +0800

    make it configurable

--- health_check.py
+++ health_check.py
@@ -3,28 +3,38 @@ import subprocess
 import time
 import logging
 import requests
+import ConfigParser
 
 # 对tomcat应用进行健康检查，连续三次健康url检查失败，则执行重启操作
 
+# 读取配置文件，设置全局基本配置
+cf = ConfigParser.ConfigParser()
+cf.read('./config')
+
 # global variables
 # log日志文件所在的位置
-log_file = './app_check.log'
+log_file = cf.get('log', 'log_file')
+# log日志的输出level
+log_level = getattr(logging, cf.get('log', 'log_level'), 'INFO')
 # 健康度检查的url
-check_url = 'http://localhost:8080/main/checkpreload.htm'
+check_url = cf.get('base', 'check_url')
 #check_url = 'http://localhost:8080/task/login/authAjax'
 # 最大重试次数，当健康度检查的url连续不成功达到最大重试次数时，执行重启等操作
-retry_times = 3
+retry_times = cf.getint('base', 'retry_times')
 # 每次健康度检查之间的间隔
-delay_sec = 5
+delay_sec = cf.getint('base', 'delay_sec')
 # 服务重启后，重新开始健康度检查的时间间隔
-delay_sec_restart = 60
+delay_sec_restart = cf.getint('base', 'delay_sec_restart')
 # http请求的超时时间，请求响应的时长超过该事件，即认定健康度检查失败
-timeout = 1
+timeout = cf.getint('base', 'timeout')
+# 健康度检查失败时所需要执行的脚本
+fail_command = cf.get('base', 'fail_command')
 
 # basic config
-logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-#logging.basicConfig(filename=log_file, level=logging.INFO)
-fail_times = 0 
+logging.basicConfig(filename=log_file, level=log_level, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+# local variables
+fail_times = 0
 
 # monitor script
 while 1:
@@ -36,7 +46,7 @@ while 1:
     if r.status_code != 200:
       logging.warning('request is not 200: %d' % (r.status_code))
       hasError = True
-  except requests.exceptions.RequestException as e: 
+  except requests.exceptions.RequestException as e:
     logging.warning(e)
     hasError = True 
   
@@ -47,7 +57,7 @@ while 1:
       logging.error('reach max try times, trying to restart application...')
       # restart server
       logging.debug('trying to restart server')
-      resp = subprocess.call(['bash', 'tomcat_restart.sh'])
+      resp = subprocess.call(['bash', fail_command])
       logging.info('=================')
       logging.debug('command response after restart server: %s' % resp)
       fail_times = 0
--- tomcat_restart.sh
+++ tomcat_restart.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 # kill tomcat process
-serverid=`ps -ef|grep apache-tomcat-7.0.82-ding-main|grep -v grep|awk '{print $2}'`
+serverpath=$1
+echo tomcat server $serverpath is restarting
+serverid=`ps -ef|grep "$serverpath" |grep -v grep|awk '{print $2}'`
 if [ "$serverid" != "" ]; then
   kill -9 $serverid
 fi
 sleep 5
-cd /usr/local/apache-tomcat-7.0.82-ding-main/bin
+cd /usr/local/$serverpath/bin
 bash startup.sh
